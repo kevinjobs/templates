80ec3ce7660ad449ec3424c0fbb6ed823dadfcc9
Kevin Jobs <me@kevinjobs.com>
2022-06-08 21:48:52 +0800

    feat(react-ts): add react-redux

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,30 @@
 /*
  * @Author       : Kevin Jobs
  * @Date         : 2022-05-09 21:26:56
- * @LastEditTime : 2022-06-07 23:15:18
+ * @LastEditTime : 2022-06-08 21:44:12
  * @lastEditors  : Kevin Jobs
  * @FilePath     : \react-ts\src\App.tsx
  * @Description  :
  */
 import React from "react";
-import './App.less';
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementByAmountAsync,
+  selectCount,
+} from "./store/slices/couter.slice";
+import store, { AppDispatch } from "./store";
+
+function AppSon() {
+  const count = useSelector(selectCount);
+  return <div>{count}</div>;
+}
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+
   const [isVisible, setIsVisible] = React.useState(false);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -21,10 +36,22 @@ function App() {
     <div className="my-app" style={{ textAlign: "center" }}>
       <h2>hello, world!</h2>
       <div>
-        <button onClick={handleClick}>show banner</button>
+        <button onClick={handleClick}>
+          {isVisible ? "hidden" : "show"} banner
+        </button>
       </div>
       <div id="banner" style={{ visibility: isVisible ? "visible" : "hidden" }}>
         <p>这个条幅可以在修改时保持状态，你可以尝试修改</p>
+      </div>
+      <p>测试 react-redux 的功能</p>
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <button onClick={() => dispatch(decrement())}>-1</button>
+      <button onClick={() => dispatch(incrementByAmount(10))}>+10</button>
+      <button onClick={() => store.dispatch(incrementByAmountAsync(20))}>
+        +20 Async (1000ms later)
+      </button>
+      <div>
+        <AppSon />
       </div>
     </div>
   );
--- react-ts/src/index.tsx
+++ react-ts/src/index.tsx
@@ -1,18 +1,21 @@
 /*
  * @Author       : Kevin Jobs
  * @Date         : 2022-05-09 21:00:03
- * @LastEditTime : 2022-06-07 23:21:49
+ * @LastEditTime : 2022-06-08 21:44:46
  * @lastEditors  : Kevin Jobs
  * @FilePath     : \react-ts\src\index.tsx
- * @Description  : 
+ * @Description  :
  */
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./App";
+import "./index.less";
 
 ReactDOM.render(
-  <div>
+  <Provider store={store}>
     <App />
-  </div>,
+  </Provider>,
   document.getElementById("root")
 );
